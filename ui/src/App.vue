1aaffdb783e29b00d9e0a9f14cd3d26bceafcdf5
György Orosz <gyorgy@orosz.link>
2025-07-21 15:37:32 +0200

    feat: file upload and making the app full screen

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <h1>Deep Chat</h1>
+  <h1>Policy align DEMO</h1>
   <!-- demo/textInput are examples of passing an object directly into a property (if this does not work refer to data values like in the history example) -->
   <!-- history is an example of passing a data object into a property -->
   <deep-chat
     :demo="true"
     :textInput="{ placeholder: { text: 'Welcome to the demo!' } }"
     :history="history"
+    :style="{
+      borderRadius: '10px',
+      width: '96vw',
+      height: 'calc(100vh - 120px)',
+      paddingTop: '10px',
+      fontSize: '1.2rem'
+    }"
+    :mixedfiles="{ files: { maxNumberOfFiles: 2, acceptedFormats: '.pdf,.md,.docx' } }"
   />
 </template>
 
