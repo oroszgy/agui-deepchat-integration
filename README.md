2a1f86139d85174feeb3f1d8554125018acc06f9
György Orosz <gyorgy@orosz.link>
2025-07-21 15:24:57 +0200

    feat: added dummy deepchat ui

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # PoC for integrating AG-UI with deepchat
 
+## Installation
+
+```bash
+uv sync
+```
+
+```bash
+npm install
+```
+
+## Usage
+Run the agent
+
+```bash
+uv run python ./agent.py
+```
+
+Serve the UI
+
+```bash
+npm run serve
+```
---
+++ ui/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Deep Chat</h1>
+  <!-- demo/textInput are examples of passing an object directly into a property (if this does not work refer to data values like in the history example) -->
+  <!-- history is an example of passing a data object into a property -->
+  <deep-chat
+    :demo="true"
+    :textInput="{ placeholder: { text: 'Welcome to the demo!' } }"
+    :history="history"
+  />
+</template>
+
+<script>
+import { ref } from 'vue';
+import 'deep-chat';
+
+export default {
+  name: 'App',
+  setup() {
+    const history = ref([
+      { role: 'user', text: 'Hey, how are you today?' },
+      { role: 'ai', text: 'I am doing very well!' },
+    ]);
+    return { history };
+  },
+};
+</script>
+
+<style>
+div {
+  font-family: sans-serif;
+  text-align: center;
+  justify-content: center;
+  display: grid;
+}
+</style>
