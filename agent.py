9eab0f594ee6c320a72c3fed72056d07ed0cb694
György Orosz <gyorgy@orosz.link>
2025-07-22 15:18:41 +0200

    feat: disabled tools

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
-from pydantic_ai import Agent, Tool
+from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 
@@ -33,10 +33,10 @@ agent = Agent(
     instructions="Be fun! You have access to two tools:"
                  "\n   1. wait: Wait for a given number of seconds"
                  "\n   2. random_number: Get a random number between specified min and max values",
-    tools=[
-        Tool(wait, name="wait", description="Wait for a given number of seconds"),
-        Tool(random_number, name="random_number", description="Get a random number"),
-    ],
+    # tools=[
+    #     Tool(wait, name="wait", description="Wait for a given number of seconds"),
+    #     Tool(random_number, name="random_number", description="Get a random number"),
+    # ],
 )
 app = agent.to_ag_ui()
 
